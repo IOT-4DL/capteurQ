949e34619087cda593edfc6d055f93fa2669ffa6
David <david@code4mac.com>
2017-04-07 22:12:28 +0200

    add hc-05 schema and software library, partially linked

--- capteurQ/capteurQ.ino
+++ capteurQ/capteurQ.ino
@@ -1,3 +1,5 @@
+
+
 /*
   capteurQ
 
@@ -6,21 +8,47 @@
 
   */
 
+  /* 
+   * HC-05 will be paired with default PIN : 1234
+   */
+
 // includes
-#include <dht.h>
+//#include <dht.h>
+#include <DHT.h>              //install dht.h and dht.cpp as library (in zip format)
+#include <SoftwareSerial.h>
 
 // defines
 #define DHT11_PIN 5
 //#define DHT22_PIN 5
+#define rxPin 13           //for D10
+#define txPin 14           //for D11
 
-dht DHT;
+//dht DHT;
+SoftwareSerial mySerial(rxPin, txPin); 
+char myChar ; 
 
 // the setup function runs once when you press reset or power the board
-void setup() {
-
+void setup() 
+{
+  Serial.begin(9600);   
+  Serial.println("Serial ...");
+  mySerial.begin(9600);
+  mySerial.println("Software Serial...");
 }
 
 // the loop function runs over and over again forever
-void loop() {
+void loop() 
+{
+  while(mySerial.available())
+  {
+    myChar = mySerial.read();
+    Serial.print(myChar);
+  }
+
+  while(Serial.available())
+  {
+    myChar = Serial.read();
+    mySerial.print(myChar);
+  }
 
 }
